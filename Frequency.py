c0afe8f5d34c781ba04338aa761c7c29add59d0a
DESKTOP-22SGGUG\xiang <once9125@gmail.com>
2017-03-13 18:46:06 +0800

    update frequency , add condition

--- Frequency.py
+++ Frequency.py
@@ -1,4 +1,5 @@
 from operator import itemgetter
+
 def list2freqdict(mylist):
     mydict=dict()
     for ch in mylist:
@@ -12,3 +13,15 @@ print(chlist)
 chfreqdict=list2freqdict(chlist)
 chfreqsorted=sorted(chfreqdict.items(),key=itemgetter(1),reverse=True)
 print(chfreqsorted)
+
+# 限定印出前幾個最高次數的字及次數
+## 僅印出前五個
+chfreqsorted2=chfreqsorted[:5]
+## 印出符合條件之結果
+chfreqsorted3=list()
+for (ch,num) in chfreqsorted:
+    if num > 1:
+        chfreqsorted3.append((ch,num))
+
+print(chfreqsorted2)
+print(chfreqsorted3)
