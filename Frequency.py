cef90c790d398f04795776ad780f23d25c801497
DESKTOP-22SGGUG\xiang <once9125@gmail.com>
2017-03-13 18:52:06 +0800

    add bigram trigram frequency

--- Frequency.py
+++ Frequency.py
@@ -12,6 +12,18 @@ def list2bigram(mylist):
 def list2trigram(mylist):
     return [mylist[i:i+3] for i in range(0,len(mylist)-2)]
 
+def bigram2freqdict(mybigram):
+    mydict=dict()
+    for (ch1,ch2) in mybigram:
+        mydict[(ch1,ch2)]=mydict.get((ch1,ch2),0)+1
+    return mydict
+
+def trigram2freqdict(mytrigram):
+    mydict=dict()
+    for (ch1,ch2,ch3) in mytrigram:
+        mydict[(ch1,ch2,ch3)]=mydict.get((ch1,ch2,ch3),0)+1
+    return mydict
+
 sentence='吃葡萄不吐葡萄皮，不吃葡萄倒吐葡萄皮。'
 chlist=[ch for ch in sentence]
 
@@ -35,5 +47,11 @@ for (ch,num) in chfreqsorted:
 
 chbigram=list2bigram(chlist)
 chtrigram=list2trigram(chlist)
-print(chbigram)
-print(chtrigram)
+# print(chbigram)
+# print(chtrigram)
+
+## 印出 bigram trigram 頻率
+bigramfreqdict=bigram2freqdict(chbigram)
+trigramfreqdict=trigram2freqdict(chtrigram)
+print(bigramfreqdict)
+print(trigramfreqdict)
