fcbd419761f1a4ddf667d9a2ee2f8809c556809e
DESKTOP-22SGGUG\xiang <once9125@gmail.com>
2017-03-13 18:56:06 +0800

    freqnency update all

--- Frequency.py
+++ Frequency.py
@@ -1,28 +1,45 @@
 from operator import itemgetter
 
+# 取單一字元算頻率
 def list2freqdict(mylist):
     mydict=dict()
     for ch in mylist:
         mydict[ch]=mydict.get(ch,0)+1
     return mydict
 
+## 產生 bigram
 def list2bigram(mylist):
     return [mylist[i:i+2] for i in range(0,len(mylist)-1)]
 
+## 產生trigram
 def list2trigram(mylist):
     return [mylist[i:i+3] for i in range(0,len(mylist)-2)]
 
+## 計算 bigram 頻率
 def bigram2freqdict(mybigram):
     mydict=dict()
     for (ch1,ch2) in mybigram:
         mydict[(ch1,ch2)]=mydict.get((ch1,ch2),0)+1
     return mydict
 
+## 計算 trigram 頻率
 def trigram2freqdict(mytrigram):
     mydict=dict()
     for (ch1,ch2,ch3) in mytrigram:
         mydict[(ch1,ch2,ch3)]=mydict.get((ch1,ch2,ch3),0)+1
     return mydict
+
+## final print
+def freq2report(freqlist):
+    chs=str()
+    print('Char(s)\tCount')
+    print('=============')
+    for (token,num) in freqlist:
+        for ch in token:
+            chs=chs+ch
+        print(chs,'\t',num)
+        chs=''
+    return
 
 sentence='吃葡萄不吐葡萄皮，不吃葡萄倒吐葡萄皮。'
 chlist=[ch for ch in sentence]
@@ -59,5 +76,11 @@ trigramfreqdict=trigram2freqdict(chtrigram)
 ## 進行排序並取出前五筆
 bigramfreqsorted=sorted(bigramfreqdict.items(), key=itemgetter(1), reverse=True)
 trigramfreqsorted=sorted(trigramfreqdict.items(), key=itemgetter(1), reverse=True)
-print(bigramfreqsorted[:5])
-print(trigramfreqsorted[:5])
+# print(bigramfreqsorted[:5])
+# print(trigramfreqsorted[:5])
+
+
+## 印出較好看的方式
+freq2report(chfreqsorted)
+freq2report(bigramfreqsorted)
+freq2report(trigramfreqsorted)
